926c8582570345037a24d303bcfc507a1beec966
sahar omer <somer1@my.yearupunited.org>
2026-04-27 14:22:48 -0400

    Home Screen up and running issue closed

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,33 @@
 package com.pluralsight;
 
+import com.pluralsight.ui.Console;
+
 public class Main {
     static void main(){
-
+        String command;
+        do{
+            command = Console.promptForString("""
+                    ------Home Screen------
+                    D) Add Deposit
+                    P) Make Payment (Debit)
+                    L) Ledger
+                    X) Exit""");
+        switch (command.toUpperCase()){
+            case "D":
+              //  addDeposit();
+                break;
+            case "P":
+              //  makePayment();
+                break;
+            case "L":
+               // ledgerMenu();
+                break;
+            case "X":
+                break;
+            default:
+                System.out.println("Invalid Input Try again!");
+        }
+        }while (!command.equalsIgnoreCase("X"));
+        System.out.println("Session terminated. Vault locked. See you next time!");
     }
 }
